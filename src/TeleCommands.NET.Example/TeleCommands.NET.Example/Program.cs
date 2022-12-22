aa0e074ed6654793c3c6f185932daa32bd707062
Ma-tes <msestaubr@gmail.com>
2022-12-22 20:49:36 +0100

    Created a small example of possible use case of the new read action

--- src/TeleCommands.NET.Example/TeleCommands.NET.Example/Program.cs
+++ src/TeleCommands.NET.Example/TeleCommands.NET.Example/Program.cs
@@ -8,7 +8,16 @@ var currentProcess = Process.GetCurrentProcess();
 var handlerManager =
     new HandlerManager(new IHandler[]
     {
-        new CommandReader(currentProcess, 128),
+        new CommandReader(currentProcess, 128)
+        {
+            OnReadAction = () => 
+            {
+                Console.Write($"{DateTime.Now.ToShortTimeString()}");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write($"{CommandReader.defaultCommandSymbol} ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        },
     });
 await handlerManager.StartHandlersUpdateAsync();
 Console.ReadLine();
