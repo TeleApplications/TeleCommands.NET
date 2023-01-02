eaabb5577b46fa686b32856c7744abbaed4b77aa
Ma-tes <msestaubr@gmail.com>
2023-01-02 19:19:21 +0100

    Implemented interop kernel methods for console writer

--- src/TeleCommands.NET.API/ConsoleWriter/InteropHelper.cs
+++ src/TeleCommands.NET.API/ConsoleWriter/InteropHelper.cs
@@ -1,6 +1,18 @@
-﻿namespace TeleCommands.NET.API.ConsoleWriter
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+using TeleCommands.NET.API.ConsoleWriter.Structures;
+using TeleCommands.NET.API.ConsoleWriter.Structures.Character;
+
+namespace TeleCommands.NET.API.ConsoleWriter
 {
     internal static class InteropHelper
     {
+        [DllImport("Kernel32.dll")]
+        public static extern SafeFileHandle CreateFile(string name, uint access, uint share, uint securityAttributes,
+            FileMode disposition, int flags, uint template);
+
+        [DllImport("Kernel32.dll")]
+        public static extern bool WriteConsoleOutputW(SafeFileHandle cosnoleOutput, CharacterInformation[] charBuffer,
+            Coordination size, Coordination position, ref Rectangle writeRegion);
     }
 }
